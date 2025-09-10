94c2578f87f37d0b55b164b1303003d2337aa737
Parash027 <parashjyotiboruah514@gmail.com>
2025-09-10 21:38:51 +0530

    Equilibrium_Index.java

--- Day_5/EquilibriumIndex.java
+++ Day_5/EquilibriumIndex.java
@@ -1,23 +1,26 @@
 public class EquilibriumIndex {
-    public static int findEquilibriumIndex(int[] arr) {
-        int totalSum = 0;
-        for (int num : arr) {
-            totalSum += num;
-        }
-
-        int leftSum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int rightSum = totalSum - leftSum - arr[i];
-            if (leftSum == rightSum) {
-                return i;
-            }
-            leftSum += arr[i];
-        }
-        return -1;
-    }
-
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 2, 2};
-        System.out.println("Equilibrium Index: " + findEquilibriumIndex(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            int leftSum = 0, rightSum = 0;
+
+            
+            for (int j = 0; j < i; j++) {
+                leftSum += arr[j];
+            }
+
+            
+            for (int j = i + 1; j < arr.length; j++) {
+                rightSum += arr[j];
+            }
+
+            if (leftSum == rightSum) {
+                System.out.println("Equilibrium Index: " + i);
+                return;
+            }
+        }
+
+        System.out.println("No Equilibrium Index found");
     }
 }
